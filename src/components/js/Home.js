fc856fae6ddd849caa8b1347d0afe683d9dd582f
djob195 <djorellanab@outlook.com>
2018-09-05 00:34:12 -0600

    jugando con bootstrap

--- src/components/js/Home.js
+++ src/components/js/Home.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import  '../css/home.css';
+
+var fondo1 = {
+    backgroundImage:"url(" + process.env.PUBLIC_URL + "/img/home2.jpg)"
+};
+
+
+var fondo2 = {
+    backgroundImage:"url(" + process.env.PUBLIC_URL + "/img/home3.jpg)"
+};
 
 export default class Home extends React.Component{
     render(){
         return(
-            <div>
-                <h3>Home</h3>
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 no-padding">
+                <div className="item-wrap" style={fondo1}></div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 no-padding">
+                    <div className="item-wrap" style={fondo2}></div>
+                </div>
             </div>
         );
     }
